6b3377b8f8b6e9aa278b190c2a631dc928e79c5c
NadiaStyslo <sir.ihor@gmail.com>
2024-03-16 11:33:55 -0400

    saved

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -11,10 +11,10 @@ type AllType = {
   weight: number;
 };
 
-function compare(
-  top: AllType,
-  bottom: AllType
-): Pick<AllType, 'name' | 'color' | 'position' | 'weight'> {
+function compare<
+  T extends Pick<AllType, 'name' | 'color'>,
+  U extends Pick<AllType, 'position' | 'weight'>
+>(top: T, bottom: U): AllType {
   return {
     name: top.name,
     color: top.color,
